511dc0ebe97a81e1d7f7b14bbedb077f122de142
sgtcuervo305 <sgtcuervo305@gmail.com>
2025-06-04 10:28:09 -0400

    adjusted data filter to only show specified info.

--- data-filter.py
+++ data-filter.py
@@ -1,2 +1,13 @@
 import csv
 
+with open('../../quantium-repo/data/daily_sales_data_0.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+    for row in reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            if row[0] == 'pink morsel':
+                print(f'\t{row[2]} {row[3]} {row[4]}')
+                line_count += 1
