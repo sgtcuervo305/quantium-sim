8f6f6b36c2d093ae307cdc01fe79f3ba2a3a53ea
sgtcuervo305 <sgtcuervo305@gmail.com>
2025-06-04 10:35:15 -0400

    forgot to remove comments

--- data-filter.py
+++ data-filter.py
@@ -7,7 +7,7 @@ with open('../../quantium-repo/data/daily_sales_data_0.csv', 'r') as csvfile:
         if line_count == 0:
             print(f'Column names are {", ".join(row[2:5])}')
             line_count += 1
-        '''else:
+        else:
             if row[0] == 'pink morsel':
                 print(f'\t{row[2]} {row[3]} {row[4]}')
-                line_count += 1'''
+                line_count += 1
