0bcfe05b365c366bc9a7b58464e02889e56edca0
sgtcuervo305 <sgtcuervo305@gmail.com>
2025-06-04 10:45:22 -0400

    adjusted to create a csv file with the results

--- data-filter.py
+++ data-filter.py
@@ -1,13 +1,18 @@
 import csv
 
-with open('../../quantium-repo/data/daily_sales_data_0.csv', 'r') as csvfile:
+with open('../../quantium-repo/data/daily_sales_data_0.csv', 'r') as csvfile, \
+     open('filtered_results.csv', 'w', newline='') as outfile:
     reader = csv.reader(csvfile, delimiter=',')
+    writer = csv.writer(outfile)
     line_count = 0
     for row in reader:
         if line_count == 0:
+            # Write header for columns 2, 3, 4
+            writer.writerow(row[2:5])
             print(f'Column names are {", ".join(row[2:5])}')
             line_count += 1
         else:
             if row[0] == 'pink morsel':
+                writer.writerow(row[2:5])
                 print(f'\t{row[2]} {row[3]} {row[4]}')
                 line_count += 1
